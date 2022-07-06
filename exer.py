ae284bcb75214074f5962b7e7bd8dabe0be166e4
MKSmonkey1 <108722976+MKSmonkey1@users.noreply.github.com>
2022-07-06 18:34:09 +0530

    Add files via upload

---
+++ exer.py
@@ -0,0 +1,19 @@
+expenses = [ 2200,2350,2600,2130,2190]
+print(expenses)
+
+print(expenses[1]-expenses[0])
+
+print(expenses[0] +expenses[1] + expenses[2])
+
+print("did we have 2000 in any expenses", 2000 in expenses)
+
+expenses.insert(5,1980)
+
+expenses[3] = expenses[3]-200
+
+print(expenses)
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+#Server's host and port number
+host = '192.168.1.35'
+port = 23
+
+#server ko bata rha hu ki interet ka socket use karna hai or TCP steam use karmi hai
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#server ko ip or port se bind kana kyunki fir pata chalega ki server kosa hai
+server.bind((host,port))
+
+#listening for inward connevtion on port(here 23)
+server.listen()
+
+print(f"server is listening on port = {port}")
+
+#jab tak true hai connection established rahega while ni hoga to connect hote  hi disconnect ho jayega
+while True:
+    conn,addr = server.accept( )
+    print(f"connecttion socket info is {conn},\n client address is {addr}")
